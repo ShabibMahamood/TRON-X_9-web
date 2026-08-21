48d29ef98eae64498ba946d0f6e638d7e3649d03
NotGenius <shabibmahamood@gmail.com>
2026-08-21 16:45:01 +0530

    phase1: use public artwork URLs

--- data/releases.ts
+++ data/releases.ts
@@ -10,12 +10,12 @@ export type Release = {
 };
 
 export const releases: Release[] = [
-  { title: 'AFTERGLOW', type: 'Single', date: '2026', artwork: '/New%20Artworks/Afterglow.png' },
-  { title: 'METASPLOIT', type: 'Single', date: '2026', artwork: '/New%20Artworks/MetaSploit.png' },
-  { title: 'GOBUSTER', type: 'Single', date: '2026', artwork: '/New%20Artworks/Gobuster.png' },
-  { title: 'SET ME FREE', type: 'Single', date: '2026', artwork: '/New%20Artworks/SetMeFree.jpg' },
-  { title: 'ON & ON', type: 'Single', date: '2026', artwork: '/New%20Artworks/On%20%26%20On%20(1).png' },
-  { title: '12 HOURS', type: 'Single', date: '2026', artwork: '/New%20Artworks/12HouRPNG.png' }
+  { title: 'AFTERGLOW', type: 'Single', date: '2026', artwork: '/artworks/afterglow.png' },
+  { title: 'METASPLOIT', type: 'Single', date: '2026', artwork: '/artworks/metasploit.png' },
+  { title: 'GOBUSTER', type: 'Single', date: '2026', artwork: '/artworks/gobuster.png' },
+  { title: 'SET ME FREE', type: 'Single', date: '2026', artwork: '/artworks/set-me-free.jpg' },
+  { title: 'ON & ON', type: 'Single', date: '2026', artwork: '/artworks/on-and-on.png' },
+  { title: '12 HOURS', type: 'Single', date: '2026', artwork: '/artworks/12-hours.png' }
 ];
 
 export const latestRelease = releases[0];
